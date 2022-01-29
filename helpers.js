5661af52c2ad94a3df2a110c58e855080809e5e8
Joshuva-ser <96343347+Joshuva-ser@users.noreply.github.com>
2022-01-29 19:45:59 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Miss Helena*:  ```" + msg + "```"
+    return "✅ *Joshuvaser*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Miss Helena*:  ```" + msg + "```"
+    return "🛑 *Joshuvaser*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Miss Helena*:  ```" + msg + "```"
+    return "⏺️ *Joshuvaser*:  ```" + msg + "```"
 }
 
 
